29830117f7e22761a58b2434f0ce86e57120803b
Jack Humphrey <jack@indeed.com>
2017-02-14 22:50:03 -0600

    First pass at docker container for instance running Imhotep frontends (IQL and IUpload) and shard builderi cron job

---
+++ frontend/tsvConverter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+lockfile -r 0 /tmp/tsvConverter.lock || exit 1
+
+export CLASSPATH="/opt/imhotepTsvConverter/shardBuilder/lib/*:/opt/imhotepTsvConverter/conf:"$CLASSPATH
+
+BUILD=/user/root/imhotep-build
+DATA=/user/root/imhotep-data
+
+java -Xmx20G com.indeed.imhotep.builder.tsv.TsvConverter --index-loc $BUILD/iupload/tsvtoindex --success-loc $BUILD/iupload/indexedtsv --failure-loc $BUILD/iupload/failed --data-loc $DATA/ --build-loc /var/data/build
+
+# Remove the lockfile
+rm -f /tmp/tsvConverter.lock
